fa431207c8efe996f695c640e6237c9d4e2b43dd
DNY <dannylu1999@gmail.com>
2022-12-18 14:48:34 +0100

    Initial commit

---
+++ src/main/java/com/example/backendeksamen2022/BackendEksamen2022Application.java
@@ -0,0 +1,13 @@
+package com.example.backendeksamen2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEksamen2022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendEksamen2022Application.class, args);
+    }
+
+}
